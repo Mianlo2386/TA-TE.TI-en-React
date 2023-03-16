47e579f2fd55b997751e37349a6a2636242e36c6
Miguel Lopez <miguellopez32@gmail.com>
2023-03-15 21:56:50 -0300

    Primer commit de TATETI

---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+export const Square = ({ children, isSelected, updateBoard, index}) => {
+    const className = `square $ {isSelected ? 'isSelected : ''}`
+    const handleClick = ()=> {
+      updateBoard(index)
+    }
+  
+    return (
+      <div onClick ={handleClick} className={className}>
+        {children}
+      </div>
+    )
+  }
